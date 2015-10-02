dae70cad1c557e113cfd2b45c58431ac391db070
Meurdok <decanter1u@fst-ats-309-l11.fst.site.univ-lorraine.fr>
2015-10-02 11:34:33 +0200

    MaJ main

--- main.c
+++ main.c
@@ -2,20 +2,27 @@
 #include <stdio.h>
 #include <SDL/SDL.h>
 
+int colorkey;
+
 
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Surface *zombie = NULL, *ecran = NULL;
-    SDL_Rect spZombie;
+    SDL_Surface *player, *temp, *screen = NULL;
+    SDL_Rect rcSrc, rcSprite;
 
-    spZombie.x = 0;
-    spZombie.y = 0;
-    ecran = SDL_SetVideoMode(1184, 666, 32, SDL_HWSURFACE);
+    rcSrc.x = 0;
+    rcSrc.y = 0;
+
+    rcSprite.x = 0;
+    rcSprite.y = 0;
+
+    screen = SDL_SetVideoMode(1184, 666, 32, SDL_HWSURFACE);
     SDL_WM_SetCaption("Trivial Zombie", NULL);
+
     int continuer = 1;
     SDL_Event event;
-    zombie = SDL_LoadBMP("zombie.bmp");
+
     while (continuer)
     {
         SDL_WaitEvent(&event);
@@ -24,7 +31,16 @@ int main(int argc, char *argv[])
             case SDL_QUIT:
             continuer = 0;
         }
-        SDL_BlitSurface(zombie, NULL, ecran, &spZombie);
+
+        temp   = SDL_LoadBMP("player.bmp");
+        player = SDL_DisplayFormat(temp);
+        colorkey = SDL_MapRGB(screen->format, 255, 0, 221);
+        SDL_SetColorKey(player, SDL_SRCCOLORKEY | SDL_RLEACCEL, colorkey);
+        SDL_FreeSurface(temp);
+
+        SDL_BlitSurface(player, &rcSrc, screen, &rcSprite);
+        SDL_Flip(screen);
+
     }
     SDL_Quit();
     return 0;
