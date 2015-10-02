50fe687b3381bb79b796c1ca4c2a9f8401faa529
Clément <k.clement.koch@gmail.com>
2015-10-02 11:07:47 +0200

    4

---
+++ definition.h
@@ -0,0 +1,62 @@
+#ifndef GENERATION_H
+#define GENERATION_H
+#include "SDL.h"
+
+/*Definition variables*/
+
+
+
+/*Definition des objets*/
+
+typedef struct object{
+
+
+    SDL_Surface *sprite;
+    int sprite_size;
+    SDL_Rect Rcsprite, Rcsource;
+    int speed;
+    int x;
+    int y;
+    int trans;
+    int life;
+
+}object;
+
+typedef struct liste{
+
+    struct object head;
+    struct liste* tail;
+
+}liste;
+
+typedef struct level{
+
+    int nbr_zombie;
+
+}level;
+
+typedef struct liste_level{
+
+    struct level head;
+    struct liste_level* tail;
+
+}liste_level;
+
+/*Definition des fonctions*/
+
+liste* insert(object obj, liste* L);
+void freelist(liste* L);
+liste freeObj(liste* L);
+
+/*Definition objets*/
+
+object zombie;
+object wall;
+object door;
+object fence;
+object bullet;
+object player;
+
+/*Definition des spawn */
+
+object spawn_zombie();
